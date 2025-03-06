9b3d8e0ef4f2f3048dd6f68dfc38cd409bee2fb0
quiroz136 <marcoantonio135@hotmail.com>
2025-03-06 18:01:20 -0400

    solo tailwind

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import Cruz from './components/icons/Cruz.jsx'
+import Moon from './components/icons/Moon.jsx'
+
+function App() {
+  
+
+  return (
+    <>    
+        <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain bg-gray-300 min-h-screen">
+          <header className="container mx-auto px-4 pt-8" >  
+            <div className="flex justify-between">
+              <h1 className="text-center uppercase text-white text-3xl font-bold -tracking-normal" >ToDo</h1>
+              <button><Moon fill="#fff"/></button>
+            </div>
+          
+          <form className="bg-white rounded-md overflow-hidden py-4 flex gap-4 items-center px-4 mt-8" >
+            <span className="rounded-full border-2 w-5 h-5 inline-block border-gray-300"></span>
+            <input type="text" placeholder="ingrese tarea..." className="w-full text-gray-400 outline-none" />
+          </form>
+          </header>
+          <main className="container mx-auto  mt-8 px-4"> 
+            <div className="bg-white rounded-md "> 
+              <article className="flex gap-4 px-4 py-4 border-b-gray-400 border-b">
+                <button className="rounded-full border-2 w-5 h-5 inline-block border-gray-300"></button>
+                <p className="text-gray-600 grow">Completar online tarea</p>
+                <button> <Cruz/></button>
+              </article>
+              <article className="flex gap-4 px-4 py-4 border-b-gray-400 border-b">
+                <button className="rounded-full border-2 w-5 h-5 inline-block border-gray-300"></button>
+                <p className="text-gray-600 grow">Completar online tarea</p>
+                <button> <Cruz/></button>
+              </article>
+              <article className="flex gap-4 px-4 py-4 border-b-gray-400 border-b">
+                <button className="rounded-full border-2 w-5 h-5 inline-block border-gray-300"></button>
+                <p className="text-gray-600 grow">Completar online tarea</p>
+                <button> <Cruz/></button>
+              </article>
+
+            <section className="py-4 px-4 flex justify-between">
+              <span className="text-gray-400">5 items left</span>
+              <button className="text-gray-400">Clear Completed</button>
+            </section>
+            </div>
+            
+          </main>
+          <section className="mx-auto container px-4 ">
+            <div className='bg-white mt-8 p-4 flex justify-center gap-4 rounded-md'> 
+              <button className='text-blue-600'>All</button>
+              <button className='hover:text-blue-600'>Active</button>
+              <button className='hover:text-blue-600'>Completed</button>
+            </div>
+            
+          </section>
+          <p className="text-center mt-8">Drag and Drop</p>
+        </div>
+      
+      
+    </>
+  )
+}
+
+export default App
