f68b72505f419fc038e9804bb6b09d443a8823cf
quiroz136 <marcoantonio135@hotmail.com>
2025-03-06 20:58:20 -0400

    video021

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import Cruz from './components/icons/Cruz.jsx'
 import Moon from './components/icons/Moon.jsx'
 
-function App() {
-  
-
+function App() { 
   return (
     <>    
         <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain bg-gray-300 min-h-screen">
@@ -12,14 +10,14 @@ function App() {
               <h1 className="text-center uppercase text-white text-3xl font-bold -tracking-normal" >ToDo</h1>
               <button><Moon fill="#fff"/></button>
             </div>
-          
+                    
+          </header>
+          <main className="container mx-auto  mt-8 px-4"> 
           <form className="bg-white rounded-md overflow-hidden py-4 flex gap-4 items-center px-4 mt-8" >
             <span className="rounded-full border-2 w-5 h-5 inline-block border-gray-300"></span>
             <input type="text" placeholder="ingrese tarea..." className="w-full text-gray-400 outline-none" />
           </form>
-          </header>
-          <main className="container mx-auto  mt-8 px-4"> 
-            <div className="bg-white rounded-md "> 
+            <div className="bg-white rounded-t-md mt-8"> 
               <article className="flex gap-4 px-4 py-4 border-b-gray-400 border-b">
                 <button className="rounded-full border-2 w-5 h-5 inline-block border-gray-300"></button>
                 <p className="text-gray-600 grow">Completar online tarea</p>
@@ -34,13 +32,12 @@ function App() {
                 <button className="rounded-full border-2 w-5 h-5 inline-block border-gray-300"></button>
                 <p className="text-gray-600 grow">Completar online tarea</p>
                 <button> <Cruz/></button>
-              </article>
-
-            <section className="py-4 px-4 flex justify-between">
+              </article>            
+            </div>
+            <section className="py-4 px-4 flex justify-between bg-white rounded-b-md" >
               <span className="text-gray-400">5 items left</span>
               <button className="text-gray-400">Clear Completed</button>
             </section>
-            </div>
             
           </main>
           <section className="mx-auto container px-4 ">
@@ -48,8 +45,7 @@ function App() {
               <button className='text-blue-600'>All</button>
               <button className='hover:text-blue-600'>Active</button>
               <button className='hover:text-blue-600'>Completed</button>
-            </div>
-            
+            </div>            
           </section>
           <p className="text-center mt-8">Drag and Drop</p>
         </div>
